18b6a88dab81ca8f9dfee538a3bb2a5a7445fa5e
Ali Shayan <160169226+Alishayan25@users.noreply.github.com>
2024-11-07 20:28:35 +0500

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+// next.config.mjs
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  trailingSlash: true,
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx', 'md'] 
+};
+
+export default nextConfig;
